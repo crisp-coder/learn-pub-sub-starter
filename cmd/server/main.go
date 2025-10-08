eb7bdb0269ced293505f9443071270a22aa03fbe
crisp-coder <crisp-coder@proton.me>
2025-10-08 09:46:19 -0500

    Added stomp, cmd/server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	connectionStr := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connectionStr)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+	fmt.Println("connection succesful")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("closing program")
 }
